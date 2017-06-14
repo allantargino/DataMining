564904266e5601b5ab248fc5cf340290c56795f4
Allan Targino <altargin@microsoft.com>
2017-06-14 11:08:23 -0300

    basic frequency count in naive-bayes algorithm

--- Algorithms/Classification/naive_bayes.py
+++ Algorithms/Classification/naive_bayes.py
@@ -1,4 +1,18 @@
-from numpy import genfromtxt
+from numpy import genfromtxt, unique
+
+data = genfromtxt('../../Data/IPR_filtered.csv', delimiter=',', skip_header=1, dtype=None)
+predict = ['Events - Organization', 'Microsoft Azure - Infra']
+
+names=unique(data[:,2])
+technologies = unique(data[:,1])
+for name in names:
+    technology = [t[1] for t in data if t[2] == name]
+    count = float(len(technology))
+    tech_unique, tech_count = unique(technology, return_counts=True)
+    tech_freq = tech_count/count
+  
+    # activity = [t[0] for t in data if t[2] == name]
+    # print name
 
 
-data = genfromtxt('../../Data/IPR.csv', delimiter=',', skip_header=1, dtype=None)
+#['altargin@microsoft.com', ['Events', 0.001], ['Azure', 0.020 ]
