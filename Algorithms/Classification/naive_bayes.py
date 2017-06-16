2ba2ebacba204a9faec4148d40fe67bb033c29b1
Allan Targino <altargin@microsoft.com>
2017-06-16 01:37:44 -0300

    naive bayes model creation and prediction without additive smoothing

--- Algorithms/Classification/naive_bayes.py
+++ Algorithms/Classification/naive_bayes.py
@@ -1,18 +1,62 @@
-from numpy import genfromtxt, unique
+from numpy import genfromtxt, unique, zeros, prod
 
-data = genfromtxt('../../Data/IPR_filtered.csv', delimiter=',', skip_header=1, dtype=None)
-predict = ['Events - Organization', 'Microsoft Azure - Infra']
+def Create_Model(data_from_gen, index_classified):
+    attributes = data_from_gen[0]
+    data = data_from_gen[1:]
+    classes = unique(data[:,index_classified])
+    model = []
+    for index_attr, attr in enumerate(attributes):
+        attr = attributes[index_attr]
+        model.append([attr])
+        attr_occur = unique(data[:,index_attr])
+        if index_attr!=index_classified:
+            for index_class, c in enumerate(classes):
+                attr_data_class = [d[index_attr] for d in data if d[index_classified] == c]
+                attr_len = float(len(attr_data_class))
+                attr_unique, attr_count = unique(attr_data_class, return_counts=True)
+                attr_freq = attr_count/attr_len
+                attr_zip = zip(attr_unique, attr_freq)
+                model[index_attr].append([c, attr_zip])
+                attr_occur_class = unique(attr_data_class)
+                diff = list(set(attr_occur) - set(attr_occur_class))
+                if len(diff) > 0:
+                    zero_freq = zip(diff,zeros(len(diff)))
+                    model[index_attr][index_class + 1][1].extend(zero_freq)
+        else:
+            attr_data_class = data[:,4]
+            attr_len = float(len(attr_data_class))
+            attr_unique, attr_count = unique(attr_data_class, return_counts=True)
+            attr_freq = attr_count/attr_len
+            attr_zip = zip(attr_unique, attr_freq)
+            model[index_attr].append(attr_zip)
+    return model
 
-names=unique(data[:,2])
-technologies = unique(data[:,1])
-for name in names:
-    technology = [t[1] for t in data if t[2] == name]
-    count = float(len(technology))
-    tech_unique, tech_count = unique(technology, return_counts=True)
-    tech_freq = tech_count/count
-  
-    # activity = [t[0] for t in data if t[2] == name]
-    # print name
+def Predict(item, model, index_classified):
+    i = 1
+    result = []
+    for name, prob in model[index_classified][1]:
+        prob_class = prob
+        for index, attr in enumerate(item):
+            class_freq = model[index][i][1]
+            attr_prob = class_freq[[x[0] for x in class_freq].index(attr)][1]
+            prob_class*= attr_prob
+        result.append((name, prob_class))
+        i=i+1
+    return result
+
+def Normalize(result):
+    norm_result = []
+    prob_sum = sum([x[1] for x in result])
+    for name, prob in result:
+        norm_result.append((name, prob/prob_sum))
+    return norm_result
 
 
-#['altargin@microsoft.com', ['Events', 0.001], ['Azure', 0.020 ]
+
+data_from_gen= genfromtxt('../../Data/weather.csv', delimiter=',', dtype=None)
+index_classified = 4 # Column index which contains classes to be classified
+item = ["Sunny", "Cool", "High","True"]
+
+model = Create_Model(data_from_gen, index_classified)
+prediction = Predict(item, model, index_classified)
+norm_prediction = Normalize(prediction)
